688e2dc074fe3d276e51460d9ae315a55672284b
gpopoiu <gpopoiu@bitdefender.com>
2022-02-23 16:35:53 +0200

    update

--- Lab1/Laborator1/Source.cpp
+++ Lab1/Laborator1/Source.cpp
@@ -1,7 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int main() {
-    printf("Hello world!\n");
-    printf("Hello world again!");
+    FILE* f = fopen("in.txt", "r");
+    if (f == NULL) {
+        printf("Cannot open\n");
+        return 0;
+    }
+    char str[128];
+    while(!feof(f)) {
+        fscanf(f, "%s", str);
+        printf("%s\n", str);
+    }
+    fclose(f);
+
+    char my_str[128];
+    printf("Introdu sirul: ");
+    scanf("%s", my_str);
+
     return 0;
 }
